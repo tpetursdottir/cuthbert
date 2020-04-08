1fea47631fa3627a1eb58f9809acb7a063f04f27
Thorbjorg <thorbjorg.petursdottir@team-consulting.com>
2020-04-08 18:35:15 +0000

    add test_video_with_rectangle

--- test_camera/test_draw_contour.py
+++ test_camera/test_draw_contour.py
@@ -23,17 +23,10 @@ cnts = imutils.grab_contours(cnts)
 
 # loop over the contours
 for c in cnts:
-    # compute the center of the contour
-    M = cv2.moments(c)
-    cX = int(M["m10"] / M["m00"])
-    cY = int(M["m01"] / M["m00"])
     
     # draw the contour and center of the shape on the image
     cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
-    cv2.circle(image, (cX, cY), 7, (255, 255, 255), -1)
-    cv2.rectangle(image,(384,0),(510,128),(0,255,0),3)
-    cv2.putText(image, "center", (cX - 20, cY - 20),
-        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+    cv2.rectangle(image,(350,80),(500,120),(0,255,0),3)
     
     # show the image
     cv2.imshow("Image", image)
---
+++ test_camera/test_video_with_rectangle.py
@@ -0,0 +1,62 @@
+# USAGE
+# python pi_surveillance.py --conf conf.json
+
+# import the necessary packages
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import datetime
+import imutils
+import time
+import cv2
+
+# Define constants
+conf = {
+        "camera_warmup_time": 2.5,
+        "resolution": [1280, 720],
+        "fps": 30
+        }
+
+# initialize the camera and grab a reference to the raw camera capture
+camera = PiCamera()
+camera.resolution = tuple(conf["resolution"])
+camera.framerate = conf["fps"]
+rawCapture = PiRGBArray(camera, size=tuple(conf["resolution"]))
+
+# allow the camera to warmup, then initialize the average frame, last
+# uploaded timestamp, and frame motion counter
+print("[INFO] warming up...")
+time.sleep(conf["camera_warmup_time"])
+avg = None
+
+# capture frames from the camera
+for f in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+    # grab the raw NumPy array representing the image and initialize
+    # the timestamp and movement/no movement text
+    frame = f.array
+    timestamp = datetime.datetime.now()
+
+    # if the average frame is None, initialize it
+    if avg is None:
+        print("[INFO] starting background model...")
+        avg = gray.copy().astype("float")
+        rawCapture.truncate(0)
+        continue
+        
+    # draw rectangle as reference on video
+    cv2.rectangle(frame,(350,80),(500,120),(0,255,0),3)
+    
+    # draw the text and timestamp on the frame
+    ts = timestamp.strftime("%A %d %B %Y %I:%M:%S%p")
+    cv2.putText(frame, ts, (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX,
+        0.35, (0, 0, 255), 1)
+
+    # display the security feed
+    cv2.imshow("Cuthbert camera", frame)
+    key = cv2.waitKey(1) & 0xFF
+
+    # if the `q` key is pressed, break from the lop
+    if key == ord("q"):
+        break
+
+    # clear the stream in preparation for the next frame
+    rawCapture.truncate(0)
