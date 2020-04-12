2ac68562d9e8e08ef4ac55873624e5985833d27e
tpetursdottir <thorbjorg.petursdottir@team-consulting.com>
2020-04-12 13:49:31 +0000

    add needle motion detector

---
+++ needle_motion_detector/needle_motion_detector.py
@@ -0,0 +1,101 @@
+# USAGE
+# python pi_surveillance.py --conf conf.json
+
+# import the necessary packages
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import argparse
+import datetime
+import imutils
+import json
+import time
+import cv2
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-c", "--conf", required=True,
+    help="path to the JSON configuration file")
+args = vars(ap.parse_args())
+
+# initialize the camera and grab a reference to the raw camera capture
+camera = PiCamera()
+camera.resolution = tuple(conf["resolution"])
+camera.framerate = conf["fps"]
+rawCapture = PiRGBArray(camera, size=tuple(conf["resolution"]))
+
+upper_left = tuple(conf["upper_left"])
+bottom_right = tuple(conf["bottom_right"])
+
+# allow the camera to warmup, then initialize the average frame, last
+# uploaded timestamp, and frame motion counter
+print("[INFO] warming up...")
+time.sleep(conf["camera_warmup_time"])
+avg = None
+
+# capture frames from the camera
+for f in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+    # grab the raw NumPy array representing the image and initialize
+    # the timestamp and movement/no movement text
+    frame = f.array
+    timestamp = datetime.datetime.now()
+    text = "No movement"
+
+    region_of_interest = frame[upper_left[1] : bottom_right[1], upper_left[0] : bottom_right[0]]
+
+    gray = cv2.cvtColor(region_of_interest, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (21, 21), 0)
+
+    # if the average frame is None, initialize it
+    if avg is None:
+        print("[INFO] starting background model...")
+        avg = gray.copy().astype("float")
+        rawCapture.truncate(0)
+        continue
+
+    # accumulate the weighted average between the current frame and
+    # previous frames, then compute the difference between the current
+    # frame and running average
+    cv2.accumulateWeighted(gray, avg, 0.5)
+    frameDelta = cv2.absdiff(gray, cv2.convertScaleAbs(avg))
+    
+    # threshold the delta image, dilate the thresholded image to fill
+    # in holes, then find contours on thresholded image
+    thresh = cv2.threshold(frameDelta, conf["delta_thresh"], 255,
+        cv2.THRESH_BINARY)[1]
+    thresh = cv2.dilate(thresh, None, iterations=2)
+    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+        cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+
+    # loop over the contours
+    for c in cnts:
+        # if the contour is too small, ignore it
+        if cv2.contourArea(c) < conf["min_area"]:
+            continue
+
+        # compute the bounding box for the contour, draw it on the frame,
+        # and update the text
+        (x, y, w, h) = cv2.boundingRect(c)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (100, 50, 200), 2)
+        text = "Movement"
+
+    # draw rectangle as reference on video
+    cv2.rectangle(frame,upper_left,bottom_right,(0,255,0),2)
+    
+    # draw the text and timestamp on the frame
+    ts = timestamp.strftime("%A %d %B %Y %I:%M:%S%p")
+    cv2.putText(frame, "Status: {}".format(text), (10, 20),
+        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+    cv2.putText(frame, ts, (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX,
+        0.35, (0, 0, 255), 1)
+
+    # display the security feed
+    cv2.imshow("Cuthbert camera", frame)
+    key = cv2.waitKey(1) & 0xFF
+
+    # if the `q` key is pressed, break from the lop
+    if key == ord("q"):
+        break
+
+    # clear the stream in preparation for the next frame
+    rawCapture.truncate(0)
